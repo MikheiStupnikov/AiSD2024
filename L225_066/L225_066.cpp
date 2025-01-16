d413a5ad06f334bfc13db8fd474f55e17c07aec2
Aselyukov1 <selyukovanton@gmail.com>
2025-01-16 13:00:21 +0500

    Delete L225_066 directory

--- L225_066/L225_066.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-//Антон Селюков L224_063 - возведение степень числа 2 в 3 степень.
-using namespace std;
-int main ()
-{
-    int power(int a,int n);
-    cout<<power(2, 3); 
-}
--- L225_066/power.h
+++
@@ -1,6 +0,0 @@
-#ifndef POWER_H
-#define POWER_H
-//Антон селюков - заголовочный файл для функции power.cpp
-int power(int a, int n);
-
-#endif
